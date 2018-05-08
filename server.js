84c63f5bd386d50327578f70250d6198d2a24a94
Ramzi Dreessen <ramzidreessen@gmail.com>
2018-05-07 20:31:00 -0500

    small changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 // var mysql = require("mysql");
 
-// Sets up the Express App
+// Sets up the Express app
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -13,12 +13,11 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// ROUTING
+// Routing
 require('./routes/htmlRoutes')(app);
 require('./routes/apiRoutes')(app);
 
-
-// logs when app is listening, on server side
+// Logs when app is listening, on server side
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
