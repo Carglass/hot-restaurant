46eed72d640dc4d356084c275f634bf537f1e50a
Ramzi Dreessen <ramzidreessen@gmail.com>
2018-05-07 19:40:57 -0500

    small server update #3

--- server.js
+++ server.js
@@ -2,13 +2,12 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-// var PORT = process.env.PORT || 3000;
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
