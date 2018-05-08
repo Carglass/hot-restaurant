eadf436e0e64b09e50c033ea111fd42afeecdb77
Madeline Story <madeline1093@gmail.com>
2018-05-07 19:22:24 -0500

    making table and waiting constructors

---
+++ data/tableData.js
@@ -0,0 +1,10 @@
+
+
+let table = function() {
+    this.customerName = customerName;
+    this.phoneNumber = phoneNumber;
+    this.customerEmail = customerEmail;
+    this.customerID = customerID;
+};
+
+module.exports = table;
---
+++ data/waitingListData.js
@@ -0,0 +1,10 @@
+
+
+let waiting = function() {
+    this.customerName = customerName;
+    this.phoneNumber = phoneNumber;
+    this.customerEmail = customerEmail;
+    this.customerID = customerID;
+};
+
+module.exports = waiting;
